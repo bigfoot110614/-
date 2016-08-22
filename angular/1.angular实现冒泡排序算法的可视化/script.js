9e2299a660d42b0fd2c3ced750435e4e3e3acdec
bigfoot110614 <826160425@qq.com>
2016-08-22 10:56:56 +0800

    增加angular可视化的冒泡排序

---
+++ angular/1.angular实现冒泡排序算法的可视化/script.js
@@ -0,0 +1,48 @@
+var app=angular.module('anApp',[]);
+app.controller('ctrl',function ctrl($scope,$timeout){
+    var defaultData=[37,28,29,24,21,11,5,10,4,3,8,1,25,12,20,30];
+
+    $scope.reset=function(){
+        $scope.data=defaultData.map(function(i){
+            return i;
+        });
+        pointer=0;
+        checklen=$scope.data.length;
+        $scope.alertMsg="";
+
+    }
+
+
+    var sorting=function(){
+        var data=$scope.data;
+
+
+        if(data[pointer]>data[pointer+1])
+        {
+            var tmp=data[pointer];
+            data[pointer]=data[pointer+1];
+            data[pointer+1]=tmp;
+        }
+
+        pointer++;
+        if( pointer>=checklen){
+            checklen--;
+            pointer=0;
+        }
+
+        if(checklen <=0){
+            $scope.alertMsg="排序结束";
+        }else{
+            $timeout(sorting,100);
+        }
+
+    };
+
+    $scope.start=function(){
+        $timeout(sorting,100);
+
+    }
+
+    $scope.reset();
+
+})
